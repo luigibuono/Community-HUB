a4d1b5f465b0155b74f06f240b5569775697a045
Luigi Buono <luigibuono1611@gmail.com>
2024-04-22 10:19:30 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  token: 'bbd58ed4262ace9a2b8521aef820b8e9d66566bcf118231febab60266da92b9e',
+  token: '',
   user: {},
   userLink: 'https://gorest.co.in/public/v2/users',
   postLink: 'https://gorest.co.in/public/v2/posts',
